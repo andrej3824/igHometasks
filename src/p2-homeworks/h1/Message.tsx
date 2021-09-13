c8ea674a2f17587b24a75d553fcf141fe42ee8a5
andrej3824 <an.drej@mail.ru>
2021-09-13 16:21:58 +0200

    1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
-function Message() {
-    return (
-        <div>
+export type MessagePropsType = {
+    avatar: any
+    name: string
+    message: string
+    time: string
+}
 
+export function Message(props: MessagePropsType) {
+    return (
+            <div className={'message'}>
+                <h2>{props.avatar}</h2>
+                <div>{props.name}</div>
+                <div>{props.message}</div>
+                <div>{props.time}</div>
         </div>
     )
 }
 
-export default Message
+
