080de0c4f7b797951c555aa435db674252ec98b0
andrej3824 <an.drej@mail.ru>
2021-09-16 13:38:50 +0200

    1

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from './Affairs.module.css'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[]
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
+    filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -17,20 +19,33 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {
+        props.setFilter('all')
+    } // need to fix
+    const setHigh = () => {
+        props.setFilter('high')
+    }
+    const setMiddle = () => {
+        props.setFilter('middle')
+    }
+    const setLow = () => {
+        props.setFilter('low')
+    }
+
+    const btnAll = s.button + ' ' + (props.filter === 'all' ? s.active : '')
+    const btnHigh = s.button + ' ' + (props.filter === 'high' ? s.active : '')
+    const btnMiddle = s.button + ' ' + (props.filter === 'middle' ? s.active : '')
+    const btnLow = s.button + ' ' + (props.filter === 'low' ? s.active : '')
 
     return (
         <div>
 
             {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <button onClick={setAll} className={btnAll}>All</button>
+            <button onClick={setHigh} className={btnHigh}>High</button>
+            <button onClick={setMiddle} className={btnMiddle}>Middle</button>
+            <button onClick={setLow} className={btnLow}>Low</button>
         </div>
     )
 }
